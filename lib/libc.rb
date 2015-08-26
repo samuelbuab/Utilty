035876c27996d6210790ec9be52970540928cedc
samuelnian <2782363967@qq.com>
2015-08-26 10:31:25 +0800

    add libc.rb
    module libc
    c语言的内存操作函数扩展成ruby接口

---
+++ lib/libc.rb
@@ -0,0 +1,14 @@
+require 'ffi'
+
+module LibC
+  extend FFI::Library
+  ffi_lib FFI::Library::LIBC
+  
+  attach_function :malloc, [:size_t], :pointer
+  attach_function :calloc, [:size_t, :size_t], :pointer
+  attach_function :realloc, [:pointer, :size_t], :pointer
+  attach_function :free, [:pointer], :void
+  
+  attach_function :memcpy, [:pointer, :pointer, :size_t], :pointer
+  attach_function :memset, [:pointer, :int, :size_t], :pointer  
+end
